39862d52050e6ba67089b5d606eeba34adf9a35e
PSheon <pauljiang61020@gmail.com>
2023-09-20 14:32:28 +0800

    feat: updated theme config.

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,14 +10,14 @@ import Logo from "./src/components/logo";
 import Footer from "./src/components/footer";
 
 const config: DocsThemeConfig = {
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  docsRepositoryBase: "https://github.com/PSheon/Books",
   useNextSeoProps: () => ({
     titleTemplate: "Oriental Books - %s",
   }),
   head: <Head />,
   logo: <Logo />,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/PSheon/Books",
   },
   footer: {
     component: <Footer />,
