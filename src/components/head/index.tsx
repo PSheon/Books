41674b9526842fc1beb164996102cb953a6bfa7c
PSheon <pauljiang61020@gmail.com>
2023-10-24 15:23:09 +0800

    feat: added favicon.

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -8,6 +8,10 @@ function Head() {
       {/* Primary Meta Tags */}
       <title>Oriental Books</title>
       <meta name="title" content="Oriental Books" />
+      <meta name="apple-mobile-web-app-title" content="Oriental Books" />
+      <meta name="application-name" content="Oriental Books" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#000000"></meta>
       <meta
         name="description"
         content="The collection here comprises a variety of fascinating ancient texts, including original content and translations"
@@ -40,8 +44,27 @@ function Head() {
       />
       <meta property="twitter:image" content="/seo/banner.webp" />
 
-      {/* Favicon */}
+      {/* Icons */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/seo/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/seo/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/seo/favicon-16x16.png"
+      />
       <link rel="manifest" href="/manifest.webmanifest" />
+      <link rel="mask-icon" href="/seo/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     </Fragment>
   );
 }
