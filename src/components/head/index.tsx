a9463dc69174a703797b7ff2f6f83a47193d468f
PSheon <pauljiang61020@gmail.com>
2023-10-24 14:57:17 +0800

    feat: added seo tags.

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -6,7 +6,39 @@ function Head() {
     <Fragment>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       {/* Primary Meta Tags */}
-      {/* TODO: Fill seo tags here */}
+      <title>Oriental Books</title>
+      <meta name="title" content="Oriental Books" />
+      <meta
+        name="description"
+        content="The collection here comprises a variety of fascinating ancient texts, including original content and translations"
+      />
+      <meta name="keywords" content="ancient texts, books, collection" />
+      <meta name="robots" content="index, follow" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="author" content="PSheon" />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://oriental-books.vercel.app/" />
+      <meta property="og:title" content="Oriental Books" />
+      <meta
+        property="og:description"
+        content="The collection here comprises a variety of fascinating ancient texts, including original content and translations"
+      />
+      <meta property="og:image" content="/seo/banner.webp" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://oriental-books.vercel.app/"
+      />
+      <meta property="twitter:title" content="Oriental Books" />
+      <meta
+        property="twitter:description"
+        content="The collection here comprises a variety of fascinating ancient texts, including original content and translations"
+      />
+      <meta property="twitter:image" content="/seo/banner.webp" />
     </Fragment>
   );
 }
